da61381e9ddbdb3337008e516954bac21436472d
Mukesh Tiwari <mukeshtiwari200102@gmail.com>
2022-01-06 16:22:33 +0545

    defined class for node and elem

---
+++ notebooks/ele.py
@@ -0,0 +1,12 @@
+#class for nodes
+class node:
+   
+    # init method or constructor 
+    def __init__(self, num, len , area , ym , theta, node_a , node_b):
+        self.num = num
+        self.len = len
+        self.area = area
+        self.ym = ym
+        self.theta = theta
+        self.node_a = node_a 
+        self.node_b = node_b
