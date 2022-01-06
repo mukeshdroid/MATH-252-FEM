a53776e1dbcf8d01908717bf365cb1fc73ecb22e
Mukesh Tiwari <mukeshtiwari200102@gmail.com>
2022-01-06 22:44:49 +0545

    started notebook implementation of truss using classes

--- notebooks/ele.py
+++ notebooks/ele.py
@@ -1,10 +1,11 @@
 #class for nodes
-class node:
+class ele:
    
     # init method or constructor 
-    def __init__(self, num, len , area , ym , theta, node_a , node_b):
+    #nan values are used to indicate if the value needs to be determined.
+    def __init__(self, num, length , area , ym , theta, node_a , node_b):
         self.num = num
-        self.len = len
+        self.len = length
         self.area = area
         self.ym = ym
         self.theta = theta
