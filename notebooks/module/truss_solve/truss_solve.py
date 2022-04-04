3b6e6024ec049871f2bc2dbaf06190e95b2168bb
Mukesh Tiwari <mukeshtiwari200102@gmail.com>
2022-04-04 18:38:59 +0545

    implemented solve and visualize in module.

--- notebooks/module/truss_solve/truss_solve.py
+++ notebooks/module/truss_solve/truss_solve.py
@@ -1,5 +1,10 @@
-import ele,node
+from node import node
+from ele import ele
 import turtle
+import numpy as np
+import math
+import copy
+
 class truss:
 
     node_list = []
@@ -26,34 +31,17 @@ class truss:
 #             #include append to list
 #             pass
 
-        node1 = node.nodep(1,-4,0)
-        node2 = node.nodep(2,-2,0)
-        node3 = node.nodep(3,0,0)
-        node4 = node.nodep(4,2,0)
-        node5 = node.nodep(5,4,0)
-        node6 = node.nodep(6,4,2.9118)
-        node7 = node.nodep(7,2,2.1842)
-        node8 = node.nodep(8,0,1.4561)
-        node9 = node.nodep(9,-2,0.7280)
+        node1 = node(1,0,0,0,0,np.nan,np.nan)
+        node2 = node(2,2,0,0,0,np.nan,np.nan)
+        node3 = node(3,2,2,np.nan,np.nan,0.1,-0.2)
 
-        self.node_list = [node1,node2,node3,node4,node5,node6,node7,node8,node9,node1]
+        self.node_list = [node1,node2,node3]
 
-        ele1  = ele.elep(1, node1, node2)
-        ele2  = ele.elep(2, node2, node3)
-        ele3  = ele.elep(3, node3, node4)
-        ele4  = ele.elep(4, node4, node5)
-        ele5  = ele.elep(5, node5, node6)
-        ele6  = ele.elep(6, node6, node7)
-        ele7  = ele.elep(7, node7, node8)
-        ele8  = ele.elep(8, node8, node9)
-        ele9  = ele.elep(9, node9, node1)
-        ele10 = ele.elep(10, node2, node9)
-        ele11 = ele.elep(11, node2, node8)
-        ele12 = ele.elep(12, node3, node8)
-        ele13 = ele.elep(13, node4, node8)
-        ele14 = ele.elep(14, node4, node7)
-        ele15 = ele.elep(15, node4, node6)
-        self.ele_list = [ele1, ele2, ele3, ele4, ele5, ele6, ele7, ele8, ele9, ele10, ele11, ele12, ele13, ele14, ele15]
+        ele1 = ele(1,1,1,1, 0 , node1 , node2)
+        ele2 = ele(2,1,1, 1 , math.pi / 2 , node2 , node3)
+        ele3 = ele(3,1*math.sqrt(2),1, 1 , math.pi / 4 , node1 , node3)
+
+        self.ele_list = [ele1, ele2, ele3]
     
     def globalstiff(self):
         #dimension of global matrix
@@ -82,6 +70,7 @@ class truss:
         #generate global matrix by calling globalstiff method
         self.globalstiff()        
         
+        dis_list = []
         for n in self.node_list:
             dis_list.append(n.dis_x)
             dis_list.append(n.dis_y)
@@ -89,7 +78,7 @@ class truss:
         dis = np.array(dis_list)
 
         f_list = []
-        for n in nodes:
+        for n in self.node_list:
             f_list.append(n.f_x)
             f_list.append(n.f_y)
 
@@ -97,12 +86,12 @@ class truss:
         
         #check for undetermined values in dis and create linear eqns
 
-        GK_dis =  copy.deepcopy(GK)
+        GK_dis =  copy.deepcopy(self.GK)
         f_dis =  copy.deepcopy(f)
 
         del_row = []
-        index_list = list(range(0,2*len(nodes)))
-        for i in range(0,2*len(nodes)):
+        index_list = list(range(0,2*len(self.node_list)))
+        for i in range(0,2*len(self.node_list)):
             if dis_list[i] == 0:
                 del_row.append(i)
                 index_list.remove(i)
@@ -114,10 +103,18 @@ class truss:
         
         for i in range(0 , len(ans_dis)):
             dis[index_list[i]] = ans_dis[i]
-            self.dis_list = dis
+        self.dis_list = dis
         
-        ans_force = np.dot(GK,dis)
+        ans_force = np.dot(self.GK,dis)
         self.force_list = ans_force
+        
+        k = 0
+        for i in self.node_list:
+            i.dis_x = self.dis_list[k]
+            k = k  + 1
+            i.dis_y = self.dis_list[k]
+            k = k  + 1
+              
         
         print(self.dis_list)
         print(self.force_list)
@@ -139,6 +136,19 @@ class truss:
             t.goto(node.pos_x*70, node.pos_y*70)
             t.pendown()
             t.dot(15,"red") 
+        #turtle.Screen().exitonclick()
+        
+        #after solving
+        t.pencolor("green")        
+        for ele in self.ele_list:
+            t.goto((ele.node_a.pos_x + ele.node_a.dis_x)*70, (ele.node_a.pos_y + ele.node_a.dis_y)*70)
+            t.goto((ele.node_b.pos_x + ele.node_b.dis_x)*70, (ele.node_b.pos_y + ele.node_b.dis_y)*70)
+        
+        for node in self.node_list:
+            t.penup()
+            t.goto((node.pos_x + node.dis_x)*70, (node.pos_y  + node.dis_y)*70)
+            t.pendown()
+            t.dot(15,"yellow") 
         turtle.Screen().exitonclick()
 
 truss1 = truss()
